3c2a12781406547122a5e204bcfd9f89985530f0
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-06-11 17:42:44 +0300

    Внесены изменения в личный проект Пока все дома

--- js/main.js
+++ js/main.js
@@ -1,80 +1,77 @@
 'use strict';
 
-function randomInteger(min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;
-}
+// обозначаем константы и переменные
+var photos = [];
+var COMMENTS = ['Всё отлично', 'В целом всё неплохо. Но не всё',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var AUTHORS = ['Петр', 'Василий', 'Дарина', 'Герман', 'Анна'];
+var NUMBER_PHOTOS = 25;
+var users = [];
 
-function trunCate(str, maxlength) {
-  if (str.length > maxlength) {
-    return str.slice(0, maxlength - 3) + '...';
-    // итоговая длина равна maxlength
-  }
-  return str;
-}
 // находим контейнер с фото - тег <template> с классом pictures и внутри ccылку с id picture, в которой одно изображение
 var similarListElement = document.querySelector('.pictures');
 var similarPhotoTemplate = document.querySelector('#picture')
     .content
     .querySelector('.picture');
 
-// обозначаем массивы и переменные
-var photos = [];
-var comments = ['Всё отлично', 'В целом всё неплохо. Но не всё',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var authors = ['Петр', 'Василий', 'Дарина', 'Герман', 'Анна'];
-var avatars = [];
-var numberPhotos = 25;
-// var randComment = comments[Math.floor(Math.random() * comments.length)];
+function randomInteger(min, max) {
+  var rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
+}
 
-// функция для создания массива с данными польвателя (имя и аватар)
-var renderAvatar = function (names, icons) {
-  for (var i = 0; i < names.length; i++) {
+// функция для создания массива с данными пользователя (имя, аватар, комментарии)
+var generateUsersData = function (names) {
+  for (var i = 0; i < AUTHORS.length; i++) {
     var userElement = {
       url: 'img/' + (i + 1) + '.svg',
-      name: names[i]
+      name: AUTHORS[i],
+      masage: COMMENTS[Math.floor(Math.random() * COMMENTS.length)]
     };
-    icons[i] = userElement;
+    names[i] = userElement;
   }
-  return userElement;
+  return names;
 };
-
-renderAvatar(authors, avatars);
+generateUsersData(users);
 
 // функция для создания массива с данными фотографии (имя пользователя, аватар, url фото, комментарии
-var renderPhotoInfo = function (blocks, text, icons) {
-  for (var i = 0; i < numberPhotos; i++) {
-    var randAuthor = Math.floor(Math.random() * icons.length);
+var generatePhotosData = function (count) {
+  for (var i = 0; i < count; i++) {
+    var randAuthor = Math.floor(Math.random() * users.length);
     var block = {
-      author: icons[randAuthor].name,
-      icon: icons[randAuthor].url,
       url: 'photos/' + (i + 1) + '.jpg',
-      comment: text[Math.floor(Math.random() * text.length)]
+      likes: randomInteger(15, 200),
+      comments: [
+        users[randAuthor]
+      ]
     };
-    blocks[i] = block;
+    photos[i] = block;
   }
-  return block;
+  return photos;
 };
 
-renderPhotoInfo(photos, comments, avatars);
+generatePhotosData(NUMBER_PHOTOS);
 
 // функция для создания блока с фото, передаем конкретное фото + количество лайков
 var renderPhoto = function (photo) {
   var photoElement = similarPhotoTemplate.cloneNode(true);
   photoElement.querySelector('.picture__img').src = photo.url;
-  photoElement.querySelector('.picture__comments').textContent = trunCate(photo.comment, 8);
-  photoElement.querySelector('.picture__likes').textContent = randomInteger(15, 200);
+  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes;
   return photoElement;
 };
 
-var fragment = document.createDocumentFragment();
-
 // передаем параметры нашего массива в функцию, вставляем фотографии ...
-for (var i = 0; i < photos.length; i++) {
-  fragment.appendChild(renderPhoto(photos[i]));
-}
-similarListElement.appendChild(fragment);
+
+var renderPhotos = function (array) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(renderPhoto(array[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+renderPhotos(photos);
