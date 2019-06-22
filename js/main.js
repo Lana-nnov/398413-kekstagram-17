2006e4dc6bc0a410eddf13090e9f6b91be0aaf48
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-06-22 23:08:41 +0300

    Начата работа над валидацией формы

--- js/main.js
+++ js/main.js
@@ -30,9 +30,11 @@ var imgPreview = imgPreviewContainer.querySelector('img');
 var fieldsetElement = popup.querySelector('.img-upload__effects');
 var levelEffect = popup.querySelector('.effect-level__pin');
 var levelEffectDepth = popup.querySelector('.effect-level__depth');
+var userCommentTextarea = popup.querySelector('.text__description');
 var STEP = 25;
 var MIN_SCALE_VALUE = 25;
 var MAX_SCALE_VALUE = 100;
+var commentFocused = false;
 
 function randomInteger(min, max) {
   var rand = min + Math.random() * (max + 1 - min);
@@ -84,7 +86,7 @@ var renderPhoto = function (photo) {
 
 
 var onPopupEscPress = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
+  if (evt.keyCode === ESC_KEYCODE && !commentFocused) {
     onButtonClose();
   }
 };
@@ -189,3 +191,14 @@ levelEffect.addEventListener('mouseup', function (evt) {
   evt.preventDefault();
   applyFilter(50);
 });
+
+// проверяем фокус на поле комментария
+userCommentTextarea.addEventListener('focus', function () {
+  commentFocused = true;
+});
+
+userCommentTextarea.addEventListener('focusout', function () {
+  commentFocused = false;
+});
+
+
