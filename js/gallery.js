c65367c9c8ca243e2ff4f894feba797544284840
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-06-29 12:13:42 +0300

    Начинаем реализацию взаимодействия с удаленным сервером

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,17 @@
 'use strict';
 (function () {
   // обозначаем константы и переменные
-  var NUMBER_PHOTOS = 25;
   var similarListElement = document.querySelector('.pictures');
   var similarPhotoTemplate = document.querySelector('#picture')
       .content
       .querySelector('.picture');
   var uploadFileElement = document.querySelector('.img-upload__start');
+  var mainPage = document.querySelector('main');
+
+  // находим блок с сообщением об ошибке
+  var errorElement = document.querySelector('#error')
+      .content
+      .querySelector('.error');
 
   // функция для создания блока с фото, передаем конкретное фото + количество лайков
   var renderPhoto = function (photo) {
@@ -18,7 +23,6 @@
   };
 
   // передаем параметры нашего массива в функцию, вставляем фотографии ...
-
   var renderPhotos = function (array) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < array.length; i++) {
@@ -27,11 +31,35 @@
     similarListElement.appendChild(fragment);
   };
 
-  var photos = window.generatePhotosData(NUMBER_PHOTOS);
-  renderPhotos(photos);
-
   // открываем popup
   uploadFileElement.addEventListener('change', function () {
     window.openPopup();
   });
+
+  // в случае успешного ответа сервера загружаем фотографии с данными
+  var onWindowLoadSuccess = function (array) {
+    renderPhotos(array);
+  };
+
+  // изменяем сообщение об ошибке для случая, если не пришли данные с сервера
+  var changeButtonError = function () {
+    var buttonErrorLast = document.querySelector('.error__buttons > button:last-child');
+    var buttonError = document.querySelector('.error__button');
+    var errorTitle = document.querySelector('.error__title');
+    errorTitle.textContent = 'Ошибка загрузки данных';
+    buttonErrorLast.classList.add('hidden');
+    buttonError.addEventListener('click', function () {
+      location.reload();
+    });
+  };
+
+  // в случае неуспешного ответа сервера показываем окно об ошибке
+  var onWindowLoadError = function () {
+    var fragment = document.createDocumentFragment();
+    fragment.appendChild(errorElement);
+    mainPage.appendChild(fragment);
+    changeButtonError();
+  };
+
+  window.load(onWindowLoadSuccess, onWindowLoadError);
 })();
