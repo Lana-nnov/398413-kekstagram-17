f9e9952412aac0e254c57918cd2e0b8226b13164
Svetlana Oshepkova <lana_nnov@mail.ru>
2019-06-30 23:32:42 +0300

    Внесены измненения в задание Личный проект: надо подкачаться

--- js/status.js
+++ js/status.js
@@ -3,12 +3,13 @@
   var mainPage = document.querySelector('main');
 
   // находим блок с сообщением об ошибке
-  var errorElement = document.querySelector('#error')
+  var errorTemplate = document.querySelector('#error')
       .content
       .querySelector('.error');
 
   // в случае неуспешного ответа сервера показываем окно об ошибке
   window.showErrorOfLoad = function (message) {
+    var errorElement = errorTemplate.cloneNode(true);
     errorElement.querySelector('.error__buttons > button:last-child').classList.add('hidden');
     errorElement.querySelector('.error__title').textContent = message;
     errorElement.querySelector('.error__button').addEventListener('click', function () {
